584265c0eab59e4e5dd2e5e118737cdc613a167f
rolansy <ronalshoeygeorge@gmail.com>
2023-10-28 17:59:33 +0530

    Restaurant Cipher Task Solution

---
+++ Restaurant Cipher.py
@@ -0,0 +1,28 @@
+
+n=int(input("Enter Number of Lines : "))
+l=[]
+def get_key_with_highest_value(dictionary):
+    max_key = None
+    max_value = None
+    
+    for key, value in dictionary.items():
+        if max_value is None or value > max_value:
+            max_key = key
+            max_value = value
+    
+    return max_key
+for i in range(n):
+    a=input("Enter String : ")
+    l.append(a)
+for i in l:
+    d={}
+    for j in i:
+        if j!=" ":
+            if j not in d.keys():
+                d[j]=1
+            else:
+                d[j]+=1
+    
+    result = get_key_with_highest_value(d)
+    print(result.upper(),end="\n")
+
